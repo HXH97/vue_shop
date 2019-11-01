fc794a5cd9945c36db6580424829211afd735f8d
han_xinghao <964047469@qq.com>
2019-11-01 18:56:17 +0800

    搭建环境并实现登录页面

---
+++ src/components/Login.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像区域 -->
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <!-- 登录表单区域 -->
+      <el-form :model="loginForm" label-width="0px" class="login_form">
+        <el-form-item>
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            type="password"
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="btn_box">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      // model属性为了表单验证
+      loginForm: {
+        username: 'admin',
+        password: '123456'
+      }
+    }
+  },
+  methods: {}
+}
+</script>
+<style scoped lang="less">
+.login_container {
+  background-color: #2b4b6b;
+  height: 100%;
+  .login_box {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    width: 450px;
+    height: 300px;
+    background-color: #fff;
+    border-radius: 3px;
+    transform: translate(-50%, -50%);
+    .avatar_box {
+      position: absolute;
+      top: 0;
+      left: 50%;
+      width: 130px;
+      height: 130px;
+      padding: 10px;
+      border: 1px solid #ccc;
+      background-color: #fff;
+      border-radius: 50%;
+      transform: translate(-50%, -50%);
+      box-shadow: 0 0 10px #ddd;
+      img {
+        width: 100%;
+        height: 100%;
+        border-radius: 50%;
+        background-color: #eee;
+      }
+    }
+    .login_form {
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      padding: 0 20px;
+      box-sizing: border-box;
+      .btn_box {
+        display: flex;
+        justify-content: flex-end;
+      }
+    }
+  }
+}
+</style>
